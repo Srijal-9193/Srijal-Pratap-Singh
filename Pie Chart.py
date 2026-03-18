43d2913cddc6f5c34ad200896b887f31a9ab2dfa
Srijal Pratap Singh <srajalsingh2002@gmail.com>
2026-03-18 13:51:26 +0530

    Delete Pie Chart.py

--- Pie Chart.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-expenses = ["Rent", "Food", "Travel", "Entertainment", "Savings"]
-amount = [4000, 2500, 1200, 800, 1500]
-
-plt.pie(amount, labels=expenses, autopct="%1.1f%%")
-plt.title("Monthly Expense Distribution")
-plt.show()
